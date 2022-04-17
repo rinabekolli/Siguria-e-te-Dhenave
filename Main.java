d56a0a7b26311098ae6622709f34e749d0206ac4
Qendresa-Potoku <94839915+Qendresa-Potoku@users.noreply.github.com>
2022-04-17 15:30:15 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,3 +3,47 @@ package siguri;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+       System.out.print("\n---Ky është algoritmi i enkriptimit/deshifrimit të Beale Ciphers që përdor një dokument teksti si libër!---\n");
+
+      
+       System.out.print("\nJu mund të shkruani encrypt për enkriptim, decrypt për dekriptim ose stop për të ndaluar programin që funksionon: ");
+       String answer = input.nextLine().toLowerCase();
+       answer = answer.replaceAll(" ", "");
+       if (args.length == 0) {
+       
+           if (answer.equals("encrypt")) {
+               System.out.println("\nJu zgjodhët enkriptimin\n");
+               System.out.print("Futni tekstin që dëshironi të kriptoni: ");
+               String plaintext = input.nextLine().toLowerCase();
+               System.out.println(Encrypt.encrypt(plaintext, readFile()));
+           } else if (answer.equals("decrypt")) {
+               System.out.println("\nJu zgjodhët dekripitimin\n");
+               System.out.print("Futni tekstin që dëshironi të dekriptoni:");
+               String ciphertext = input.nextLine().toLowerCase();
+              System.out.println(Decrypt.decrypt(ciphertext, readFile()));
+           } else if (answer.equals("stop")) {
+          System.out.println("\nProgrami po ndalon...");
+       
+           }
+         
+           }
+          
+           input.close();
+       }
+       
+   
+
+
+   public static String readFile() {
+
+       String addNumber = "";
+
+       try {
+           File file = new File("/Users/qendr/OneDrive/Desktop/Siguri_Projekt2/book.txt");
+           Scanner fileReader = new Scanner(file);
