49eb90c9d8b07df1870c515ec51c4a8d4a2779d9
RinesaImeri <95044677+RinesaImeri@users.noreply.github.com>
2022-04-17 15:23:32 +0200

    Update Encrypt.java

--- Encrypt.java
+++ Encrypt.java
@@ -1,4 +1,22 @@
 package siguri;
-
 import java.util.ArrayList;
 import java.util.regex.Pattern;
+
+
+public class Encrypt {
+
+	public static String  encrypt(String plaintext, String key) {
+	
+		 if (plaintext.length() == 0) {
+	            return "\nJu duhet të shkruani tekstin e thjeshtë që dëshironi të enkriptoni.";
+	        }
+
+	        if (Pattern.matches("[a-zA-Z0-9 ]+", plaintext)) {
+	            char[] plaintextArray = plaintext.replaceAll(" ", "").toCharArray();
+	            String[] splitedKey = key.split(" ");
+
+	            StringBuilder getLetters = new StringBuilder();
+	            for (int i = 0; i < splitedKey.length; i++) {
+	                getLetters.append(splitedKey[i].charAt(0));
+	            }
+
