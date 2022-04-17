006f893b5e559809151e9d20a13d242c2890fa27
rinabekolli <94354965+rinabekolli@users.noreply.github.com>
2022-04-17 15:39:22 +0200

    Update Decrypt.java

--- Decrypt.java
+++ Decrypt.java
@@ -10,3 +10,23 @@ public class Decrypt {
 	        if (Pattern.matches("[0-9 ]+", ciphertext)) {
 	            String[] cipherArray = ciphertext.split(" ");
 	            String[] splitKey = key.split(" ");
+
+		    StringBuilder plaintext = new StringBuilder();
+	            for (int i = 0; i < cipherArray.length; i++) {
+	                for (int j = 0; j < splitKey.length; j++) {
+	                    if (Integer.parseInt(cipherArray[i]) > splitKey.length) {
+	                        return "\nNumri " + cipherArray[i] + " është invalid. Maksimumi i parashikuar është " + splitKey.length + ".";
+	                    }
+	                    if (cipherArray[i].equals(String.valueOf(j + 1))) {
+	                        plaintext.append(splitKey[j].charAt(0));
+	                    }
+	                }
+	            }
+	            return "\nTeksti i dekriptuar: " + (plaintext);
+	        } else {
+	            return "\nCiphertext-i që dëshironi të dekriptoni duhet të përmbajë vetëm numra!";
+	        }
+	    }
+	
+	
+	}
