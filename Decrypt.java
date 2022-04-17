3028e27b71bbf973d63b4f2b742c7e1ac998643f
rinabekolli <94354965+rinabekolli@users.noreply.github.com>
2022-04-17 15:34:32 +0200

    Update Decrypt.java

--- Decrypt.java
+++ Decrypt.java
@@ -1,2 +1,12 @@
 package siguri;
 import java.util.regex.Pattern;
+
+public class Decrypt {
+
+	public static String decrypt(String ciphertext, String key) {
+		 if (ciphertext.length() == 0) {
+	            return "\nJu duhet të shkruani ciphertext-in që dëshironi të dekriptoni!";
+	        }
+	        if (Pattern.matches("[0-9 ]+", ciphertext)) {
+	            String[] cipherArray = ciphertext.split(" ");
+	            String[] splitKey = key.split(" ");
